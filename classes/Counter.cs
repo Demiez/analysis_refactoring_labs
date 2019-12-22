e810cfc882496520d45c96d6c1f69c09709e23a1
Demiez <dmg.petrenko@gmail.com>
2019-12-22 11:29:29 +0200

    did 1st lab

---
+++ classes/Counter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class Counter
+    {
+        //State
+        public int x; // значение
+        public int hx; // шаг
+
+        // Конструктор без параметров
+        public Counter()
+        {
+            // внутри конструктора можем установить кастомные дефолтные значения, или им будут присвоены дефолтные для типа данных(у нас int)
+            x = 1;
+            hx = 10;
+        }
+
+        // Конструктор с одним параметром (начальное значение счетчика)
+        public Counter(int initx)
+        {
+            x = initx;
+        }
+
+        // Конструктор с двумя параметрами (начальное значение и шаг)
+        public Counter(int initx, int inithx)
+        {
+            x = initx;
+            hx = inithx;
+        }
+
+        // Методы
+        public int generateValue()
+            => x += hx;
+
+        public int increaseCounter(int constant)
+            => x += constant;
+
+        public void showState()
+            => Console.WriteLine($"Текущее состояние счетчика: {x}");
+
+
+
+    }
+}
---
+++ classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Classes ***\n");
+            Counter counter01 = new Counter(); // использовали дефолтный конструктор без параметров
+            counter01.generateValue();
+            counter01.showState(); // Ожидаем 11
+            counter01.generateValue();
+            counter01.showState(); // Ожидаем 21
+
+            Counter counter03 = new Counter(5, 10); // используем перегрузку и 3й метод
+            counter03.generateValue();
+            counter03.showState(); // Ожидаем 15
+            counter03.increaseCounter(100);
+            counter03.showState(); // Ожидаем 115
+
+            Console.ReadLine();
+        }
+    }
+}
